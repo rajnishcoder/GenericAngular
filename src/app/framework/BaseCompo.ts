f8c33bcb29969195ab0c6c76617568ac98dfb5ce
rajnishcoder <getwere@gmail.com>
2018-06-24 19:20:55 +0530

    base comp

--- src/app/framework/BaseCompo.ts
+++ src/app/framework/BaseCompo.ts
@@ -35,29 +35,29 @@ export class BaseComponent implements OnInit {
         }
     }
 
-    showCommonLoader(taskCode: TaskCode) {
-		document.getElementById('commonFullLoader').style.display = 'block';
-	}
-	hideCommonLoader(taskCode: TaskCode) {
-		document.getElementById('commonFullLoader').style.display = 'none';
-	}
+    // showCommonLoader(taskCode: TaskCode) {
+	// 	document.getElementById('commonFullLoader').style.display = 'block';
+	// }
+	// hideCommonLoader(taskCode: TaskCode) {
+	// 	document.getElementById('commonFullLoader').style.display = 'none';
+	// }
 
     onPreExecute(taskCode: TaskCode) {
         switch (taskCode) {
             default:
-                this.showCommonLoader(taskCode);
+                // this.showCommonLoader(taskCode);
                 break;
         }
     }
 
     onErrorReceived(taskCode: TaskCode) {
-        this.hideCommonLoader(taskCode);
+        // this.hideCommonLoader(taskCode);
         console.log('on error recevied of base compo');
     }
 
     onResponseReceived(taskCode: TaskCode, response: any) {
         // Logger.log(response);
-        this.hideCommonLoader(taskCode);
+        // this.hideCommonLoader(taskCode);
         if (response instanceof BaseResponse) {
             return !response.error;
         }
